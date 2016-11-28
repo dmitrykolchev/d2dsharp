a984b9e4238c1321614071ccfb4d70c434e7576f
dmitrykolchev_cp <SND\dmitrykolchev_cp>
2016-11-28 12:51:35 +0000

    new build configurations

--- D2DSharp/Managed.D2DSharp.Math/Program.cs
+++ D2DSharp/Managed.D2DSharp.Math/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Managed.D2DSharp.Math;
 using Managed.Graphics.Direct2D;
+using System.Diagnostics;
 
 namespace Managed.D2DSharp.Math
 {
@@ -23,15 +24,18 @@ namespace Managed.D2DSharp.Math
                 }
             }
             {
-
-                Console.WriteLine("{0:HH:mm:ss.fff}", DateTime.Now);
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
 
                 Matrix4x4 m4 = new Matrix4x4();
                 for (int index = 0; index < 10000000; ++index)
                 {
+                    m2.SetProduct(m2, m2);
+                    m1.SetProduct(m1, m1);
                     m4.SetProduct(m1, m2);
                 }
-                Console.WriteLine("{0:HH:mm:ss.fff}", DateTime.Now);
+                stopwatch.Stop();
+                Console.WriteLine("Elapsed time: {0} ms", stopwatch.ElapsedMilliseconds);
             }
         }
     }
